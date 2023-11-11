714383fa6eed66cec8a78b9c00d46aef21c62ef5
Marc Schär <marcschaer@Marcs-MacBook-Pro.local>
2023-11-11 06:31:13 +0100

    First Commit - Part 1 done

---
+++ part-1/FirstComponent.js
@@ -0,0 +1,7 @@
+/** My First Component
+ *
+ * @returns "My very first Component!!!""
+ */
+function FirstComponent() {
+  return <h1>My very first Component!!!</h1>;
+}
---
+++ part-1/NamedComponent.js
@@ -0,0 +1,9 @@
+/** Named Component: greets
+ *
+ * Props:
+ * name --> introduce yourself
+ */
+
+function NamedComponent({ name }) {
+  return <p> My Name is {name} </p>;
+}
